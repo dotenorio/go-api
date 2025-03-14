bb0d7a043a8ce2164103878d694e7beef3abe28f
Fernando Tenorio <dotenorio@gmail.com>
2025-03-14 13:43:26 -0300

    tests: add some tests

--- go.mod
+++ go.mod
@@ -4,9 +4,8 @@ go 1.24.1
 
 require (
 	github.com/gin-gonic/gin v1.10.0
-	github.com/lib/pq v1.10.9
-	github.com/stretchr/testify v1.9.0
 	github.com/joho/godotenv v1.5.1
+	github.com/stretchr/testify v1.9.0
 	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.25.12
 )
--- main.go
+++ main.go
@@ -13,39 +13,52 @@ import (
 	"gorm.io/gorm"
 )
 
+// User model
 type User struct {
-	Id        string         `gorm:"primaryKey;type:uuid;default:gen_random_uuid()" json:"id"`
-	Name      string         `gorm:"not null" json:"name"`
-	Email     string         `gorm:"not null" json:"email"`
-	CreatedAt time.Time      `gorm:"default:now()" json:"created_at"`
-	UpdatedAt *time.Time     `gorm:"default:null" json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+	Id        string          `gorm:"primaryKey;type:uuid;default:gen_random_uuid()" json:"id"`
+	Name      string          `gorm:"not null" json:"name"`
+	Email     string          `gorm:"not null" json:"email"`
+	CreatedAt time.Time       `gorm:"default:now()" json:"created_at"`
+	UpdatedAt *time.Time      `gorm:"default:null" json:"updated_at"`
+	DeletedAt *gorm.DeletedAt `gorm:"index;default:null" json:"deleted_at"`
 }
 
 var db *gorm.DB
 
 func main() {
+	// Chama a função que carrega as variáveis de ambiente
+	loadEnv()
+
+	// Chama a função que conecta ao banco de dados
+	setupDb()
+
+	// Chama a função que configura o router do Gin
+	router := setupRouter()
+
+	// Inicia o servidor
+	appPort := os.Getenv("API_PORT")
+	fmt.Printf("Server running on port:%s", appPort)
+	router.Run(fmt.Sprintf(":%s", appPort))
+}
+
+func loadEnv() {
 	// Carrega as variáveis de ambiente do arquivo .env
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error on load .env file.")
 	}
+}
 
-	// Acessa as variáveis de ambiente
-	appPort := os.Getenv("API_PORT")
+func setupDb() {
+	// Acessa as variáveis de ambiente para conectar ao banco
 	dbUser := os.Getenv("DATABASE_USER")
 	dbPassword := os.Getenv("DATABASE_PASSWORD")
 	dbName := os.Getenv("DATABASE_NAME")
 
-	// Exibe as variáveis de ambiente
-	fmt.Println("API_PORT:", appPort)
-	fmt.Println("DATABASE_USER:", dbUser)
-	fmt.Println("DATABASE_PASSWORD:", dbPassword)
-	fmt.Println("DATABASE_NAME:", dbName)
-
 	// Conecta ao banco de dados
 	dsn := fmt.Sprintf("host=localhost port=5432 user=%s password=%s dbname=%s sslmode=disable", dbUser, dbPassword, dbName)
 
+	var err error
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Fail on connect to database.")
@@ -56,20 +69,20 @@ func main() {
 	if err != nil {
 		panic("Fail do migrate model User.")
 	}
+}
 
+func setupRouter() *gin.Engine {
 	// Configura o router do Gin
 	router := gin.Default()
 
 	// Rotas
 	router.GET("/users", getUsers)
-	router.GET("/users/:id", getUserByID)
+	router.GET("/users/:id", getUserById)
 	router.POST("/users", postUser)
 	router.PATCH("/users/:id", patchUser)
 	router.DELETE("/users/:id", deleteUser)
 
-	// Inicia o servidor
-	fmt.Printf("Server running on port:%s", appPort)
-	router.Run(fmt.Sprintf(":%s", appPort))
+	return router
 }
 
 // Retorna todos os usuários
@@ -83,8 +96,8 @@ func getUsers(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, users)
 }
 
-// Retorna um usuário pelo ID
-func getUserByID(c *gin.Context) {
+// Retorna um usuário pelo Id
+func getUserById(c *gin.Context) {
 	id := c.Params.ByName("id")
 	var user User
 	result := db.Where("id = ? AND deleted_at IS NULL", id).First(&user)
